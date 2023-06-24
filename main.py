c76e988e0968cece202c2a6d79936d568b3a4875
JeronimasDargis <jeronimasd@gmail.com>
2023-06-24 14:41:56 -0400

    feat: function to read encrypted data

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env/ python
 from Crypto.Cipher import AES
 import optparse
-import json
+import binascii
 # from Crypto.Random import get_random_bytes
 import os
 
@@ -29,7 +29,7 @@ def write_new_password():
     text = input("Enter the text to encrypt: ")
 
 
-    filename = "encrypted_text.json"
+    filename = "encrypted_text.txt"
     if not os.path.exists(filename):
         with open(filename, 'w') as file:
             pass
@@ -39,42 +39,52 @@ def write_new_password():
 
 
 def read_password():
-    print("read password")
+    # just for testing purposes
+    key = b'\x01\x23\x45\x67\x89\xab\xcd\xef\xfe\xdc\xba\x98\x76\x54\x32\x10'
+    filename = "encrypted_text.txt"
+    search_key = 'secret'
+
+    decrypted_value = read_encrypted_text(filename, key, search_key)
+    if decrypted_value is not None:
+        print("Decrypted value:", decrypted_value)
+    else:
+        print("Key not found in the file.")
 
 
 def encrypt_text(key, text):
-
     cipher = AES.new(key, AES.MODE_EAX)
     ciphertext, tag = cipher.encrypt_and_digest(text.encode())
     return cipher.nonce + tag + ciphertext
 
 
-# def append_encrypted_text(filename, key, text):
-#     encrypted_text = encrypt_text(key, text)
-#     with open(filename, 'ab') as file:
-#         file.write(encrypted_text)
-#         file.write(b'\n')
-
-
 def append_encrypted_text(filename, key, text):
     encrypted_text = encrypt_text(key, text)
-    print(encrypted_text)
 
-    # Load existing data from the file, if any
-    try:
-        with open(filename, 'r') as file:
-            data = json.load(file)
-    except (IOError, ValueError):
-        data = {}
+    with open(filename, 'ab') as file:
+        file.write("secret:" + binascii.hexlify(encrypted_text) + "\n")
 
-    # Update the data with the new encrypted text
-    data["secret_key"] = encrypted_text
 
-    # Save the updated data to the file
-    with open(filename, 'w') as file:
-        json.dump(data, file)
+def decrypt_text(key, ciphertext):
+    nonce = ciphertext[:16]
+    tag = ciphertext[16:32]
+    encrypted_text = ciphertext[32:]
 
-    print("Text encrypted and saved to", filename)
+    cipher = AES.new(key, AES.MODE_EAX, nonce)
+    decrypted_text = cipher.decrypt_and_verify(encrypted_text, tag)
+    return decrypted_text
+
+
+def read_encrypted_text(filename, key, search_key):
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line.startswith(search_key + ":"):
+                encrypted_value = line.split(":")[1]
+                ciphertext = binascii.unhexlify(encrypted_value)
+                decrypted_value = decrypt_text(key, ciphertext)
+                return decrypted_value
+
+    return None
 
 
 def main():
