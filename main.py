07567509170801f171b4602bf77a844c60669bd8
JeronimasDargis <jeronimasd@gmail.com>
2023-06-24 13:16:09 -0400

    feat: script to take a AES key, encrypt and write to a file

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#!/usr/bin/env/ python
+from Crypto.Cipher import AES
+# from Crypto.Random import get_random_bytes
+import os
 
 
-# Press the green button in the gutter to run the script.
+# function that takes a string "password" and appends to the encrypted_password.txt file
+
+def encrypt_text(key, text):
+    print(key)
+    print(text)
+    cipher = AES.new(key, AES.MODE_EAX)
+    ciphertext, tag = cipher.encrypt_and_digest(text.encode())
+    return cipher.nonce + tag + ciphertext
+
+
+def append_encrypted_text(filename, key, text):
+    encrypted_text = encrypt_text(key, text)
+    with open(filename, 'ab') as file:
+        file.write(encrypted_text)
+        file.write(b'\n')
+
+
+def main():
+    key = input("Enter the AES key (16, 24, or 32 bytes): ")
+    text = input("Enter the text to encrypt: ")
+    # text = str(text)
+    filename = "encrypted_text.txt"
+
+    if not os.path.exists(filename):
+        with open(filename, 'w') as file:
+            pass
+
+    append_encrypted_text(filename, key, text)
+    print("Text encrypted and appended to", filename)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
