ca8bd2c4a58dd52d765b0305d719f005b2c18046
JeronimasDargis <jeronimasd@gmail.com>
2023-06-24 13:40:48 -0400

    feat: getting some basic arguments from user

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env/ python
 from Crypto.Cipher import AES
+import optparse
 # from Crypto.Random import get_random_bytes
 import os
 
 
 # function that takes a string "password" and appends to the encrypted_password.txt file
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-m", "--mode", dest="encryption_mode", help="Defining the mode of the encryption.")
+    parser.add_option("-k", "--key", dest="string_key", help="Passing the key that will be used to encrypt/decrypt passwords")
+    parser.add_option("-p", "--pass", dest="password", help="Passing the password for encryption.")
+
+    (options, arguments) = parser.parse_args()
+    if not options.encryption_mode:
+        parser.error("[-] Please specify a mode, use --help for more info.")
+    if options.encryption_mode == "write" and not options.string_key:
+        parser.error("[-] Please specify a key that will be used to encrypt your password, use --help for more info.")
+    if options.encryption_mode == "write" and not options.password:
+        parser.error("[-] Please specify a password that will be encrypted, use --help for more info.")
+    return options
+
 
 def encrypt_text(key, text):
     print(key)
@@ -22,17 +39,23 @@ def append_encrypted_text(filename, key, text):
 
 
 def main():
-    key = input("Enter the AES key (16, 24, or 32 bytes): ")
-    text = input("Enter the text to encrypt: ")
+    user_input = get_arguments()
+    if user_input.encryption_mode == "write":
+        print('user wants to write password')
+    if user_input.encryption_mode == "read":
+        print('user wants to read a password')
+
+    # key = input("Enter the AES key (16, 24, or 32 bytes): ")
+    # text = input("Enter the text to encrypt: ")
     # text = str(text)
-    filename = "encrypted_text.txt"
-
-    if not os.path.exists(filename):
-        with open(filename, 'w') as file:
-            pass
-
-    append_encrypted_text(filename, key, text)
-    print("Text encrypted and appended to", filename)
+    # filename = "encrypted_text.txt"
+    #
+    # if not os.path.exists(filename):
+    #     with open(filename, 'w') as file:
+    #         pass
+    #
+    # append_encrypted_text(filename, key, text)
+    # print("Text encrypted and appended to", filename)
 
 
 if __name__ == '__main__':
